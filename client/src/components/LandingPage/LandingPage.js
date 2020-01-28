37b28504a38036169a9281be01ca7662400e99a3
mdamian9 <m.damian9@hotmail.com>
2020-01-27 18:51:23 -0800

    Created components folder, updated App.js to use components and index.js to use react router

---
+++ client/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+const LandingPage = () => {
+    return (
+        <Container>
+            <Row className='text-center'>
+                <Col>
+                    This is the Landing Page
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default LandingPage;
